8b6cfe74c85f63586eb84e16dfcf0af57b6778b6
Athandwaclear <siyasangalakhiwe6@gmail.com>
2016-11-14 15:49:43 +0200

    I have done some changes in this file

--- sameWeekday.js
+++ sameWeekday.js
@@ -1,11 +1,8 @@
+const assert = require('assert');
 function sameWeekday(dt1, dt2) {
 var date1 = new Date();
 var date2 = new Date();
-if (date1.getDay() === date2.getDay()) {
-  return "The days are equal to each other";
-  }
-  else {
-    return "The days are not equal to each other";
-  }
+return date1.getDay() === date2.getDay();
 }
-console.log(sameWeekday("2005-09-23", "2005-09-23"));
+console.log(sameWeekday("2005-09-23", "2005-09-30"));
+assert.equal(sameWeekday("2005-09-23", "2005-09-30"), true);
